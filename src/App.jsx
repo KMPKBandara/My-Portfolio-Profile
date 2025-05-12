734453c9b4b974a4e65c587df9e5082a51a6016b
Pramuditha <pramudithakavinda01@gmail.com>
2025-05-12 22:33:54 +0530

    Add svg

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 
-// You’ll add more pages like About, Projects etc. later
-
+// ✅ Add the basename here for GitHub Pages
 function App() {
   return (
-    <Router>
+    <Router basename="/My-Portfolio-Profile">
       <Navbar />
       <div className="pt-20">
-        {" "}
         {/* Padding to avoid overlap with fixed navbar */}
         <Routes>
           <Route path="/" element={<Home />} />
